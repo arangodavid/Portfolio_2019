05ce945c6c3509855c3217a06d7a3438a7042143
David Arango <arango.david8@gmail.com>
2019-07-24 10:52:15 -0400

    Updates

---
+++ precache-manifest.c78d6ec09f48f96608f605984a2d7da3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f895caeb9cadce9fba3c05cc11d47002",
+    "url": "/Portfolio_2019/index.html"
+  },
+  {
+    "revision": "b617ca14454d1c941775",
+    "url": "/Portfolio_2019/static/css/main.67aac08d.chunk.css"
+  },
+  {
+    "revision": "a3d29448eec3afd90b8c",
+    "url": "/Portfolio_2019/static/js/2.424c27cb.chunk.js"
+  },
+  {
+    "revision": "b617ca14454d1c941775",
+    "url": "/Portfolio_2019/static/js/main.82571a82.chunk.js"
+  },
+  {
+    "revision": "96214b88ce248ddfa2c2",
+    "url": "/Portfolio_2019/static/js/runtime~main.7afb1bb9.js"
+  },
+  {
+    "revision": "bd07b24f0d571d40602e48dceea699fb",
+    "url": "/Portfolio_2019/static/media/car-seven.bd07b24f.jpg"
+  },
+  {
+    "revision": "c39b38ff5b96416089f085de2b57d012",
+    "url": "/Portfolio_2019/static/media/car.c39b38ff.svg"
+  },
+  {
+    "revision": "f107cd54b8d304e1ad88c470c9ab2cdb",
+    "url": "/Portfolio_2019/static/media/mountains.f107cd54.jpg"
+  },
+  {
+    "revision": "8c181c2b9b0b840762eb1f393ca974db",
+    "url": "/Portfolio_2019/static/media/programmer.8c181c2b.jpg"
+  },
+  {
+    "revision": "d609bb10e03acd41941f46aa20fbf39f",
+    "url": "/Portfolio_2019/static/media/snowboarding-three.d609bb10.svg"
+  },
+  {
+    "revision": "ef6a9dd6765cd32fcadaba2ea96951fd",
+    "url": "/Portfolio_2019/static/media/tree-two.ef6a9dd6.jpg"
+  },
+  {
+    "revision": "a8341aadbc72ba6fa01d5e23aa67e9c5",
+    "url": "/Portfolio_2019/static/media/tree.a8341aad.svg"
+  },
+  {
+    "revision": "3001be5b8dcf1ca10c7c4994b5216918",
+    "url": "/Portfolio_2019/static/media/woods.3001be5b.jpg"
+  }
+]);
