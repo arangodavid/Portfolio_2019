97d7dee6f2db0a198ef652f71bf132a5865ea0e4
David Arango <arango.david8@gmail.com>
2019-07-26 04:28:54 -0400

    Added footer section

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import linkedin from "../../assets/linkedin.svg";
+import github from "../../assets/github.svg";
+import codepen from "../../assets/codepen.svg";
 
 class Footer extends React.Component {
 	render() {
 		return (
 			<footer className="footer">
-				<h1>FOOTER</h1>
+				<ul className="social_icon_cont">
+					<li className="social_icon"><a className="social_icon_link" href="" target="_blank"><img src={linkedin} alt="Linkedin Icon"/></a></li>
+					<li className="social_icon"><a className="social_icon_link" href="" target="_blank"><img src={github} alt="Github Icon"/></a></li>
+					<li className="social_icon"><a className="social_icon_link" href="" target="_blank"><img src={codepen} alt="Codepen Icon"/></a></li>
+				</ul>
+				<section className="footer_notes">
+					<p className="footer_notes_p">Made from scratch with React.js</p>
+					<p className="footer_notes_p">&copy; 2019 David Arango Portfolio. All rights reserved.</p>
+				</section>
 			</footer>
 		)
 	}
